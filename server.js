f21bfbd7a3452492c80fad51a57843427f93e574
nazmul-rion <nazmulrion16@gmail.com>
2021-11-11 17:39:05 +0600

    order and cars added in server

--- server.js
+++ server.js
@@ -1,1 +1,102 @@
-Hello
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const ObjectId = require('mongodb').ObjectId;
+
+
+// API URI : 
+
+// Middleware  
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oklco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("database connection succesfully");
+        const database = client.db("TravelDB");
+        const carCollection = database.collection("cars");
+        const orderCollection = database.collection("orders");
+
+        //-----GET API-----//
+
+        // Get all cars 
+        app.get("/allcars", async (req, res) => {
+            const cursor = carCollection.find({});
+            const cars = await cursor.toArray();
+            res.send(cars);
+        });
+
+        // Get all orders 
+        app.get("/allorders", async (req, res) => {
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+
+        //-----POST API-----//
+
+        // add car API
+        app.post('/addcars', async (req, res) => {
+            const result = await carCollection.insertOne(req.body);
+            res.json(result);
+        });
+
+        // add Order API
+        app.post('/addorder', async (req, res) => {
+            const result = await orderCollection.insertOne(req.body);
+            res.json(result);
+        });
+
+
+        //-----UPDATE API-----//
+
+        // UPDATE Single ORDER by ID 
+        app.put('/allorders/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    Status: 'Confirmed'
+                }
+            };
+            const result = await orderCollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+        });
+
+
+        //-----DELETE API-----//
+
+        // Delete Single Order by ID
+        app.delete('/allorders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        });
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+
+    res.send("server is running");
+
+});
+
+app.listen(port, () => {
+    console.log(`My Server listening at http://localhost:${port}`)
+})
